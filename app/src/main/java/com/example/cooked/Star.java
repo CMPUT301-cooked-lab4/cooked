352df7bfc772849a4bbb56b4b07f7ab5f5d91093
Rebecca <rirving@ualberta.ca>
2026-01-28 18:21:31 -0700

    Add Star shape

---
+++ app/src/main/java/com/example/cooked/Star.java
@@ -0,0 +1,10 @@
+package com.example.cooked;
+
+public class Star extends Shape {
+    public int numVertices;
+
+    public Star(int x, int y, int numVertices) {
+        super(x, y);
+        this.numVertices = numVertices;
+    }
+}
