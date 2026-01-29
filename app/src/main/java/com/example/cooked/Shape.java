f4ead09e662f22491152e58c9d338152cdeacad2
Rana Sunjog Singh Thind <71564731+sunjogthind@users.noreply.github.com>
2026-01-28 18:52:09 -0700

    added a color string field

--- app/src/main/java/com/example/cooked/Shape.java
+++ app/src/main/java/com/example/cooked/Shape.java
@@ -3,7 +3,7 @@ package com.example.cooked;
 public abstract class Shape {
     public int x;
     public int y;
-
+    String color = "blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
