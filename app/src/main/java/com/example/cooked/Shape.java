caf9781b4822be1ae3fd49bd85768327a42f98b7
Rebecca <rirving@ualberta.ca>
2026-01-28 18:10:56 -0700

    New Project after nuke

---
+++ app/src/main/java/com/example/cooked/Shape.java
@@ -0,0 +1,12 @@
+package com.example.cooked;
+
+public abstract class Shape {
+    public int x;
+    public int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
