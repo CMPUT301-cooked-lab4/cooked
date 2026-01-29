9f6fd912a262bf344c95e4d1bbb243ff7b46365f
Rebecca <rirving@ualberta.ca>
2026-01-28 19:02:58 -0700

    to be resolved commit

--- app/src/main/java/com/example/cooked/Shape.java
+++ app/src/main/java/com/example/cooked/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
     public int x;
     public int y;
 
+    public String color = "yellow";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
