fe72dd6a9f7d3fc337681812a4c0f4977a189fa1
altairgy <gao28@ualberta.ca>
2026-01-30 15:28:11 -0700

    Refactor color field and add getColor method

--- app/src/main/java/com/example/cooked/Shape.java
+++ app/src/main/java/com/example/cooked/Shape.java
@@ -1,16 +1,16 @@
 package com.example.cooked;
 
-import android.graphics.Color;
-
 public abstract class Shape {
-
-    String colour = "blue";
     public int x;
     public int y;
-    String color = "blue";
+    public String color = "blue";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public String getColor() {
+        return color;
+    }
 }
